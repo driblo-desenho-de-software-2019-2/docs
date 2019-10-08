eb8cae00d5398adcff77db7c633462b5c9ad316f
Mota <jpmota.unb@gmail.com>
2019-10-07 22:47:20 -0300

    Ajustando pagina inicial
    Co-authored-by: Rafael Teodosio <rafzteodosio@gmail.com>

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -1,70 +1,362 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+const React = require("react");
 
-const React = require('react');
-
-const CompLibrary = require('../../core/CompLibrary.js');
+const CompLibrary = require("../../core/CompLibrary.js");
 
 const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
-class HomeSplash extends React.Component {
+class Membros extends React.Component {
   render() {
-    const {siteConfig, language = ''} = this.props;
-    const {baseUrl, docsUrl} = siteConfig;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
-
-    const SplashContainer = props => (
-      <div className="homeContainer">
-        <div className="homeSplashFade">
-          <div className="wrapper homeWrapper">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const Logo = props => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
-      </div>
-    );
-
-    const ProjectTitle = () => (
-      <h2 className="projectTitle">
-        {siteConfig.title}
-        <small>{siteConfig.tagline}</small>
-      </h2>
-    );
-
- 
     return (
-      <SplashContainer>
-        <Logo img_src={`${baseUrl}img/dibro-logo.png`} />
-        <div className="inner">
-          <ProjectTitle siteConfig={siteConfig} />
-          
+      <section className="faculty-area section-gap">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="section-title text-center">
+                <h1>Time de Desenvolvimento</h1>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center d-flex align-items-center">
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img className="img-fluid" src="img/faculty/caio.png" alt="" />
+              </div>
+              <div className="meta-text text-center">
+                <h4>Caio Oliveira</h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a href="https://www.github.com/caiooliv" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img
+                  className="img-fluid"
+                  src="img/faculty/byron.jpeg"
+                  alt=""
+                />
+              </div>
+              <div className="meta-text text-center">
+                <h4>Byron Kamal</h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a href="https://www.github.com/byronkamal" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img
+                  className="img-fluid"
+                  src="img/faculty/henrique.png"
+                  alt=""
+                />
+              </div>
+              <div className="meta-text text-center">
+                <h4>Henrique Martins </h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a href="https://www.github.com/Henrike100" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img
+                  className="img-fluid"
+                  src="img/faculty/matheus.png"
+                  alt=""
+                />
+              </div>
+              <div className="meta-text text-center">
+                <h4>Matheus Rodrigues</h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a href="https://www.github.com/matheus-rn" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row justify-content-center d-flex align-items-center">
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img className="img-fluid" src="img/faculty/mota.jpg" alt="" />
+              </div>
+              <div className="meta-text text-center">
+                <h4>João Pedro Mota</h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a href="https://www.github.com/jpmota2208" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img className="img-fluid" src="img/faculty/luis.png" alt="" />
+              </div>
+              <div className="meta-text text-center">
+                <h4>Luís Cláudio</h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a href="https://www.github.com/LuisCL94" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img
+                  className="img-fluid"
+                  src="img/faculty/araujo.jpeg"
+                  alt=""
+                />
+              </div>
+              <div className="meta-text text-center">
+                <h4>Marcelo Araújo</h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a href="https://www.github.com/santosm46" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img
+                  className="img-fluid"
+                  src="img/faculty/magalhaes.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="meta-text text-center">
+                <h4>Marcelo Magalhães</h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a href="https://www.github.com/marrcelo" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row justify-content-center d-flex align-items-center">
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img
+                  className="img-fluid"
+                  src="img/faculty/rafael.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="meta-text text-center">
+                <h4>Rafael Santos</h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a
+                    href="https://www.github.com/rafaelteodosio"
+                    target="_blank"
+                  >
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+              <div className="thumb d-flex justify-content-center">
+                <img
+                  className="img-fluid"
+                  src="img/faculty/samuel.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="meta-text text-center">
+                <h4>Samuel Barros</h4>
+                <p className="designation">Desenvolvedor</p>
+
+                <div className="align-items-center justify-content-center d-flex">
+                  <a href="https://www.github.com/BordaLorde" target="_blank">
+                    <i className="fa fa-github"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </SplashContainer>
+      </section>
+    );
+  }
+}
+
+class Features extends React.Component {
+  render() {
+    return (
+      <section className="feature-area">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="section-title text-center">
+                <h1>Principais funcionalidades</h1>
+              </div>
+            </div>
+          </div>
+          <div className="feature-inner row">
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-files"></i>
+                <h4>Lorem ipsum</h4>
+                <div
+                  className="wow fadeIn"
+                  data-wow-duration="1s"
+                  data-wow-delay=".3s"
+                >
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-headphone-alt"></i>
+                <h4>Lorem ipsum</h4>
+                <div
+                  className="wow fadeIn"
+                  data-wow-duration="1s"
+                  data-wow-delay=".5s"
+                >
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-crown"></i>
+                <h4>Lorem ipsum</h4>
+                <div
+                  className="wow fadeIn"
+                  data-wow-duration="1s"
+                  data-wow-delay=".1s"
+                >
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-briefcase"></i>
+                <h4>Lorem ipsum</h4>
+                <div
+                  className="wow fadeIn"
+                  data-wow-duration="1s"
+                  data-wow-delay=".3s"
+                >
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-medall-alt"></i>
+                <h4>Lorem ipsum</h4>
+                <div
+                  className="wow fadeIn"
+                  data-wow-duration="1s"
+                  data-wow-delay=".5s"
+                >
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-key"></i>
+                <h4>Lorem ipsum</h4>
+                <div
+                  className="wow fadeIn"
+                  data-wow-duration="1s"
+                  data-wow-delay=".1s"
+                >
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
 
 class Index extends React.Component {
   render() {
-    const {config: siteConfig, language = ''} = this.props;
-    const {baseUrl} = siteConfig;
+    const { config: siteConfig, language = "" } = this.props;
+    const { baseUrl } = siteConfig;
 
     const Block = props => (
       <Container
-        padding={['bottom', 'top']}
+        padding={["bottom", "top"]}
         id={props.id}
-        background={props.background}>
+        background={props.background}
+      >
         <GridBlock
           align="center"
           contents={props.children}
@@ -73,40 +365,60 @@ class Index extends React.Component {
       </Container>
     );
 
-    const Features = () => (
+    const Driblo_Features = () => (
       <Block layout="fourColumn" background="light">
         {[
           {
-            content: 'Não esquente a cabeça com o sorteio de times, o driblô cuida disso pra você.',
+            content:
+              "Não esquente a cabeça com o sorteio de times, o driblô cuida disso pra você.",
             image: `${baseUrl}img/undraw_team_spirit.svg`,
-            imageAlign: 'top',
-            title: 'Sorteio de times',
+            imageAlign: "top",
+            title: "Sorteio de times"
           },
           {
-            content: 'Marque o placar e o tempo no driblô',
+            content: "Marque o placar e o tempo no driblô",
             image: `${baseUrl}img/undraw_time_management.svg`,
-            imageAlign: 'top',
-            title: 'Gerenciamento de partidas',
+            imageAlign: "top",
+            title: "Gerenciamento de partidas"
           },
           {
-            content: 'A gente cuida do pagamento, dividindo o valor entre os membros da pelada.',
+            content:
+              "A gente cuida do pagamento, dividindo o valor entre os membros da pelada.",
             image: `${baseUrl}img/undraw_wallet.svg`,
-            imageAlign: 'top',
-            title: 'Gerenciamento de pagamentos',
-          },
-  
-
+            imageAlign: "top",
+            title: "Gerenciamento de pagamentos"
+          }
         ]}
       </Block>
     );
 
-
     return (
       <div>
-        <HomeSplash siteConfig={siteConfig} language={language} />
-        <div className="mainContainer">
-          <Features />
-         
+        <div>
+          <section className="home-banner-area relative">
+            <div className="container">
+              <div className="row fullscreen d-flex align-items-center justify-content-center">
+                <div className="banner-content col-lg-8 col-md-12">
+                  <h1 className="wow fadeIn" data-wow-duration="4s">
+                    Driblô
+                  </h1>
+                  <p className="text-white" style={{ fontSize: 30 }}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="rocket-img">
+              <img src="img/rocket.png" alt="" />
+            </div>
+          </section>
+        </div>
+        <Driblo_Features />
+        <Features />
+        <Membros />
+        <div id="back-top">
+          <a title="Go to Top" href="#"></a>
         </div>
       </div>
     );
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,53 +11,55 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: "User1",
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+    image: "/img/undraw_open_source.svg",
+    infoLink: "https://www.facebook.com",
+    pinned: true
+  }
 ];
 
 const siteConfig = {
-  title: 'Driblô', // Title for your website.
-  tagline: 'Para organizar suas partidas de futebol',
-  url: 'https://driblo-desenho-de-software-2019-2.github.io', // Your website URL
-  baseUrl: '/docs/', // Base URL for your project */
+  title: "Driblô", // Title for your website.
+  tagline: "Para organizar suas partidas de futebol",
+  url: "https://driblo-desenho-de-software-2019-2.github.io", // Your website URL
+  baseUrl: "/docs/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docs',
-  organizationName: 'driblo-desenho-de-software-2019-2',
+  projectName: "docs",
+  organizationName: "driblo-desenho-de-software-2019-2",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'richPicture', label: 'Dinâmicas e Seminários'},
-    {doc: 'sp1', label: 'Sprints'},
-    {doc: 'docVisao', label: 'Iniciativas Extras'},
-    { href: 'https://github.com/driblo-desenho-de-software-2019-2/docs', external: true ,label: 'GitHub' },
-
-  
+    { doc: "richPicture", label: "Dinâmicas e Seminários" },
+    { doc: "sp1", label: "Sprints" },
+    { doc: "docVisao", label: "Iniciativas Extras" },
+    {
+      href: "https://github.com/driblo-desenho-de-software-2019-2/docs",
+      external: true,
+      label: "GitHub"
+    }
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+  headerIcon: "img/favicon.ico",
+  footerIcon: "img/favicon.ico",
+  favicon: "img/favicon.ico",
 
   /* Colors for website */
   colors: {
-    primaryColor: '#52a94d',
-    secondaryColor: '#52a94d',
+    primaryColor: "#10C971",
+    secondaryColor: "#10C971"
   },
 
   /* Custom fonts for website */
@@ -79,20 +81,45 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: [
+    "https://buttons.github.io/buttons.js",
+    "js/vendor/jquery-2.2.4.min.js",
+    {
+      src:
+        "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js",
+      integrity:
+        "sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    },
+    "js/easing.min.js",
+    "js/hoverIntent.js",
+    "js/superfish.min.js",
+    "js/vendor/bootstrap.min.js",
+    "js/jquery.ajaxchimp.min.js",
+    "js/jquery.magnific-popup.min.js",
+    "js/owl.carousel.min.js",
+    "js/owl-carousel-thumb.min.js",
+    "js/jquery.sticky.js",
+    "js/jquery.nice-select.min.js",
+    "js/parallax.min.js",
+    "js/waypoints.min.js",
+    "js/wow.min.js",
+    "js/jquery.counterup.min.js",
+    "js/mail-script.js",
+    "js/main.js"
+  ],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: "img/undraw_online.svg",
+  twitterImage: "img/undraw_tweetstorm.svg"
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
